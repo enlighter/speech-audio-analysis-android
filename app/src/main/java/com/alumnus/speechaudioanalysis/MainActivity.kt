0b5875012f4091f2aa8d570c800d482c4d4ee783
Sushovan Mandal <sushovan@alumnux.com>
2017-10-27 18:33:44 +0530

    set up framework for button click activity using onClickListener properly in Kotlin

--- app/src/main/java/com/alumnus/speechaudioanalysis/MainActivity.kt
+++ app/src/main/java/com/alumnus/speechaudioanalysis/MainActivity.kt
@@ -2,18 +2,28 @@ package com.alumnus.speechaudioanalysis
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+//import android.widget.Button
+import android.media.MediaRecorder
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //@JvmField val mainButton = findViewById<>(R.id.main_button)
+        mainButton.setOnClickListener(this)
+
         // Example of a call to a native method
-        sample_text.text = stringFromJNI()
+        //sample_text.text = stringFromJNI()
+    }
+
+    override fun onClick(v: View?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     /**
