1e8242f09bcfc73d75bcdf9bab2d68816162adf9
Sushovan Mandal <sushovan@alumnux.com>
2017-10-30 19:58:42 +0530

    add an alternate implementation extending threading to run sound processor pararelly for real time performance

---
+++ app/src/main/java/com/alumnus/speechaudioanalysis/SoundRecorderProcessorLiveThread.kt
@@ -0,0 +1,26 @@
+package com.alumnus.speechaudioanalysis
+
+/**
+ * Created by sushovan on 30/10/17.
+ */
+
+import android.media.AudioRecord
+import android.media.MediaRecorder
+import android.media.AudioFormat
+
+class SoundRecorderProcessorLiveThread: Thread(){
+    private var stopped: Boolean
+
+    init {
+        stopped = false
+        start()
+    }
+
+    override fun run()
+    {
+        android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_URGENT_AUDIO)
+        var recorder : AudioRecord? = null
+        //2D array: val array = Array(row, {IntArray(column)}
+        var buffers= Array(256, {ShortArray(160)})
+    }
+}
