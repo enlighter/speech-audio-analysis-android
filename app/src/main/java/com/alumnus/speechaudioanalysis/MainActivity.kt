f16715a9ba1c71ed138b9acd249f3e01b079778a
Sushovan Mandal <sushovan@alumnux.com>
2017-10-31 17:18:00 +0530

    implement kotlin best practice of directly assigning text instead of using setter

--- app/src/main/java/com/alumnus/speechaudioanalysis/MainActivity.kt
+++ app/src/main/java/com/alumnus/speechaudioanalysis/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         //@JvmField val mainButton = findViewById<>(R.id.main_button)
         mainButton.setOnClickListener(this)
-        mainButton.setText(getText(R.string.buttonOFF))
+        mainButton.text = getText(R.string.buttonOFF)
 
         // Example of a call to a native method
         //sample_text.text = stringFromJNI()
@@ -34,11 +34,11 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             if(!SoundObject.isRecording())
             {
                 SoundObject.start()
-                mainButton.setText(getText(R.string.buttonON))
+                mainButton.text = getText(R.string.buttonON)
             }
             else {
                 SoundObject.stop()
-                mainButton.setText(getText(R.string.buttonOFF))
+                mainButton.text = getText(R.string.buttonOFF)
             }
         }
     }
