dbbd4539df558c5a6fb4a17afc75a637835d2c77
Sushovan Mandal <sushovan@alumnux.com>
2017-11-02 13:31:09 +0530

    add conversion test lab

---
+++ app/src/main/java/com/alumnus/speechaudioanalysis/testLab.java
@@ -0,0 +1,20 @@
+package com.alumnus.speechaudioanalysis;
+
+/**
+ * Created by sushovan on 2/11/17.
+ */
+
+import be.tarsos.dsp.pitch.PitchDetectionHandler;
+import be.tarsos.dsp.AudioEvent;
+import be.tarsos.dsp.pitch.PitchDetectionResult
+
+public class testLab {
+
+    PitchDetectionHandler handler = new PitchDetectionHandler() {
+        @Override
+        public void handlePitch(PitchDetectionResult pitchDetectionResult,
+                                AudioEvent audioEvent) {
+            System.out.println(audioEvent.getTimeStamp() + " " + pitchDetectionResult.getPitch());
+        }
+    };
+}
