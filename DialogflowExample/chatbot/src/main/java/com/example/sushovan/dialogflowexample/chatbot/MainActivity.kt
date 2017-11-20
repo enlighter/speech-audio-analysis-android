02a482dfe2082b5d7e9a43ce5bcd49aef54387ff
Sushovan Mandal <sushovan@alumnux.com>
2017-11-20 18:10:06 +0530

    complete the rest of the main activity def alongwith TTS again to kotlin

--- DialogflowExample/chatbot/src/main/java/com/example/sushovan/dialogflowexample/chatbot/MainActivity.kt
+++ DialogflowExample/chatbot/src/main/java/com/example/sushovan/dialogflowexample/chatbot/MainActivity.kt
@@ -1,16 +1,17 @@
 package com.example.sushovan.dialogflowexample.chatbot
 
+import android.content.Intent
 import android.os.Bundle
-import android.speech.tts.TextToSpeech
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
-    private val mOnNavigationItemSelectedListener =
+    /*private val mOnNavigationItemSelectedListener =
             BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
@@ -27,25 +28,61 @@ class MainActivity : BaseActivity() {
             }
         }
         false
-    }
-    private lateinit var tts: TTS
+    }*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
-        tts = TTS(this)
+        //navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        TTS.init(applicationContext)
     }
 
-    /*override fun onCreateOptionsMenu(menu: Menu): Boolean {
+    override fun onStart() {
+        super.onStart()
+
+        checkAudioRecordPermission()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
         return true
-    }*/
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id = item.getItemId()
+        if (id == R.id.action_settings) {
+            //val intent = Intent(this, AISettingsActivity::class.java)
+            startActivity(AISettingsActivity::class.java)
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
     fun serviceSampleClick(view: View) {
+        //val intent = Intent(this, AIServiceSampleActivity::class.java)
         startActivity(AIServiceSampleActivity::class.java)
+    }
+
+    /*fun buttonSampleClick(view: View) {
+        startActivity(AIButtonSampleActivity::class.java)
+    }*/
+
+    /*fun dialogSampleClick(view: View) {
+        startActivity(AIDialogSampleActivity::class.java)
+    }*/
+
+    /*fun textSampleClick(view: View) {
+        startActivity(AITextSampleActivity::class.java)
+    }*/
+
+    private fun startActivity(cls: Class<*>) {
+        val intent = Intent(this, cls)
+        startActivity(intent)
     }
 
     companion object {
