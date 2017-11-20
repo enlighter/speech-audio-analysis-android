603ada50d1df73abfb1f7ba9693593ce9c955bb3
Sushovan Mandal <sushovan@alumnux.com>
2017-11-20 17:00:22 +0530

    revert back to java version of TTS because of Kotlin singleton Context usage concern

---
+++ DialogflowExample/chatbot/src/main/java/com/example/sushovan/dialogflowexample/chatbot/TTS.java
@@ -0,0 +1,29 @@
+package com.example.sushovan.dialogflowexample.chatbot;
+
+/**
+ * Created by sushovan on 20/11/17.
+ */
+
+import android.content.Context;
+import android.speech.tts.TextToSpeech;
+
+public class TTS {
+
+    private static TextToSpeech textToSpeech;
+
+    public static void init(final Context context) {
+        if (textToSpeech == null) {
+            textToSpeech = new TextToSpeech(context, new TextToSpeech.OnInitListener() {
+                @Override
+                public void onInit(int i) {
+
+                }
+            });
+        }
+    }
+
+    public static void speak(final String text) {
+        textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
+    }
+}
+
